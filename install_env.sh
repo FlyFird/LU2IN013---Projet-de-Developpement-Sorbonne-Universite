3536ba20e5fcca8311ec15f506a811c898b19531
Zhou Jérémy <92992243+FlyFird@users.noreply.github.com>
2026-06-15 22:42:20 +0200

    Add files via upload

---
+++ install_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Création de l'environnement virtuel nommé ProjectEnv
+python3 -m venv ProjectEnv
+
+# Activation de l'environnement virtuel
+source ProjectEnv/bin/activate
+
+# Mise à jour de pip (optionnel mais recommandé)
+pip install --upgrade pip
+
+# Installation des dépendances à partir du fichier requirements.txt
+pip install -r requirements.txt
+
+echo "L'environnement virtuel et les dépendances ont été installés."
---
+++ run_app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Rend le script Python exécutable
+chmod +x ./gitlab_main/__main__.py
+
+# Navigue dans le bon répertoire
+cd gitlab_main || exit
+
+# Active l'environnement virtuel
+source ../ProjectEnv/bin/activate
+
+# Exécute le script Python avec Python 3
+python3 __main__.py
+
+# Désactive l'environnement virtuel
+deactivate
